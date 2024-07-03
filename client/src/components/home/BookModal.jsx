5a0b5618cb6dab2841a5700875c70670327c053f
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-07-03 21:16:02 +0300

    Changes to be committed: 	modified:   client/src/components/home/BookModal.jsx 	modified:   package-lock.json 	modified:   package.json

--- client/src/components/home/BookModal.jsx
+++ client/src/components/home/BookModal.jsx
@@ -1,12 +1,12 @@
 import { AiOutlineClose } from 'react-icons/ai';
 import { PiBookOpenTextLight } from 'react-icons/pi';
 import { BiUserCircle } from 'react-icons/bi';
-
+import PropTypes from 'prop-types';
 
 const BookModal = ({ book, onClose }) => {
     return (
         <div className='fixed bg-white bg-opacity-60 top-0 right-0 left-0 bottom-0 z-50 flex justify-center items-center' onClick={onClose}>
-            <div onClick={(e) => e.stopPropagation()} className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'>
+            <div onClick={(e) => e.stopPropagation()} className='w-[600px] max-w-full h-fit bg-white rounded-xl p-4 flex flex-col relative'>
                 <AiOutlineClose
                     className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
                     onClick={onClose}
@@ -28,9 +28,19 @@ const BookModal = ({ book, onClose }) => {
                 <p className='my-1 text-gray-500'>
                     {book.description}
                 </p>
+
+                <div className="flex w-full justify-between items-center mt-5">
+                    <button className='bg-green-300 text-white px-4 py-1 rounded hover:bg-green-500'>Buy</button>
+                    <button className='bg-yellow-300 text-white px-4 py-1 rounded hover:bg-yellow-500'>Borrow</button>
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
+
+BookModal.propTypes = {
+    book: PropTypes.object.isRequired,
+    onClose: PropTypes.func.isRequired
+};
 
 export default BookModal
